192e0ca141de952888264630fd06f26d4423c355
Rebecca <rebecca.kane@outlook.com>
2017-09-19 09:35:39 +0100

    Forgot about some features, done now

--- 5-GuessingGame.py
+++ 5-GuessingGame.py
@@ -8,20 +8,26 @@ print("Guess a number between 1 and 50.")
 secretNum = randint(0,50)
 # Generate random int adapted from https://stackoverflow.com/questions/3996904/generate-random-integers-between-0-and-9
 
-
 # Take input from the user and convert to int
 userGuess = int(input("Your guess: "))
+
+alreadyGuessed = []
 
 # While guess is wrong, take more inputs
 while userGuess != secretNum:
     if userGuess < secretNum:
         print("Too low, try again.")
         userGuess = int(input())
+        alreadyGuessed.append(userGuess) # Add the guess to an array
 
     elif userGuess > secretNum:
         print("Too high, try again.")
         userGuess = int(input())
+        alreadyGuessed.append(userGuess)
 
 # If guess is right, tell user
 if userGuess == secretNum:
     print("You're right!")
+    print("It took you " + str(len(set(alreadyGuessed)) + 1) + " tries.")
+    # Get number of individual elements in an array adapted from https://stackoverflow.com/questions/12282232/how-do-i-count-unique-values-inside-an-array-in-python
+    # +1 to count the right guess.
